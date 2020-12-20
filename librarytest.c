9500094245057fce8cd694246fd0b0b789a42899
Alon <alon212@gmail.com>
2020-12-20 17:07:05 +0200

    fixed segmentation fault

--- librarytest.c
+++ librarytest.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "mycode.h"
 
-int main (int argc, char **argv) {
-    if (argc > 0 ){
+int main (int argc, char *argv[]) {
+    if (argc < 2 ){
+        printf("Error, string missing\n");
+    }
+    else
+    {
         printf("%s\n",argv[1]);
         printf("%s\n",reverse(argv[1]));
-
-    }
+        
+    }    
 }
