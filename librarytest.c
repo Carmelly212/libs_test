727c1c31aa4aae54d9aa2a615d8ecb115df9dc5b
Alon <alon212@gmail.com>
2020-12-21 17:12:37 +0200

    fixed librarytest.c to allow more then one string

--- librarytest.c
+++ librarytest.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include "mycode.h"
+#include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[]) {
+    char *aggrigated;
+    int len, sum;
+    sum = 0;
+    int i=0;
+    for (i=1; i < argc; i++) {
+        len = strlen(argv[i]);
+        sum = len + sum;
+ }  
+    aggrigated = (char *) malloc(sum + 2);
+    
+    for (i=1; i < argc; i++) {
+        char * space = " ";
+        strcat(aggrigated, argv[i]);
+        strcat(aggrigated, space);
+ }  
     if (argc < 2 ){
         printf("Error, string missing\n");
     }
     else
     {
-        printf("%s\n",argv[1]);
-        printf("%s\n",reverse(argv[1]));
+        printf("You enterd: %s\n",aggrigated);
+        printf("Reversed: %s\n",reverse(aggrigated));
         
     }    
 }
